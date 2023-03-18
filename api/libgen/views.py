c48c3ca8bb4326755d4f0672e2edb9361415144d
Andrei Kovacs <andreikovacs03@gmail.com>
2023-03-18 21:12:59 +0200

    feat(libgen): download

--- api/libgen/views.py
+++ api/libgen/views.py
@@ -1,3 +1,4 @@
+import urllib.parse
 from libgen_api import LibgenSearch
 from flask import Blueprint
 
@@ -6,5 +7,12 @@ blueprint = Blueprint("libgen", __name__, url_prefix="/libgen")
 
 @blueprint.route("/search_title/<title>")
 def search_title(title: str):
-    search = LibgenSearch()
-    return search.search_title(title)
+    s = LibgenSearch()
+    return s.search_title(title)
+
+
+@blueprint.route("/download/<link>")
+def download(link: str):
+    s = LibgenSearch()
+    decoded_link = urllib.parse.unquote(link)
+    return s.resolve_download_links({"Mirror_1": decoded_link})
